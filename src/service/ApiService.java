2ba2d15e7b5c93b88910e2c16621471afc7c16d0
Felipe Alves <felipealves3580@gmail.com>
2024-06-23 17:36:44 -0300

    Implement getRequest method

---
+++ src/service/ApiService.java
@@ -0,0 +1,38 @@
+package service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+
+public class ApiService {
+    private ArrayList<String> cepList = new ArrayList<>();
+
+    public void getRequest(String address) {
+        String apiUrl = "https://viacep.com.br/ws/" + address + "/json/";
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                cepList.add(response.body());
+                System.out.println(response.body());
+            } else {
+                System.out.println("CEP inválido");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
